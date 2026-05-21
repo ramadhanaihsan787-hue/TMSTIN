c3814fa2942c8417e127eca5766a03221627cb64
Ihsan <ramadhanaihsan787@gmail.com>
2026-05-21 20:58:50 +0700

    fix: nama POD

--- Frontend/src/features/pod/pages/index.ts
+++ Frontend/src/features/pod/pages/index.ts
@@ -2,4 +2,4 @@
 export { default as HistoryPage } from './HistoryPage';
 export { default as MonitoringPage } from './MonitoringPage';
 export { default as VerificationsPage } from './VerificationsPage';
-export { default as PodSettingsPage } from './PodSettingsPage';
+export { default as PodSettingsPage } from './PODSettingsPage';
